de2fb651805f9a28c0c2f0767a247bf76d942f5e
akshat123644532 <akshatsolanki106@gmail.com>
2026-05-26 18:30:44 +0530

    define logic

--- frontent/secure-user-dashboard/src/pages/Signup.jsx
+++ frontent/secure-user-dashboard/src/pages/Signup.jsx
@@ -1,29 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Signup() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <div className="container">
       <h1>Signup Page</h1>
 
-      <form>
-        <div>
-          <label>Name</label>
-          <input type="text" placeholder="Enter your name" />
-        </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="name"
+          type="text"
+          placeholder="Enter name"
+          onChange={handleChange}
+        />
 
-        <div>
-          <label>Email</label>
-          <input type="email" placeholder="Enter your email" />
-        </div>
+        <input
+          name="email"
+          type="email"
+          placeholder="Enter email"
+          onChange={handleChange}
+        />
 
-        <div>
-          <label>Password</label>
-          <input type="password" placeholder="Enter your password" />
-        </div>
+        <input
+          name="password"
+          type="password"
+          placeholder="Enter password"
+          onChange={handleChange}
+        />
 
         <button type="submit">Signup</button>
-        
       </form>
+
+      <span>
+        Already have an account? <Link to="/login">Login</Link>
+      </span>
     </div>
   );
 }
