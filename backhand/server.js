723a6a43a99572964b150fc5005cf9e8ae700efa
akshat123644532 <akshatsolanki106@gmail.com>
2026-05-26 16:06:32 +0530

    create a middleware for token verify and use in server .js

--- backhand/server.js
+++ backhand/server.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 5050;
 
 const authRoutes = require("./routes/authRoutes");
+const authMiddleware = require("./middleware/authMiddleware");
 const connectDB = require("./config/db");
 
 app.use(cors());
@@ -17,7 +18,16 @@ app.get("/", (req, res) => {
     res.send("server is working");
 });
 
+
 app.use("/api/auth", authRoutes);
+
+
+app.get("/api/profile", authMiddleware, (req, res) => {
+    res.json({
+        message: "Protected profile data",
+        user: req.user
+    });
+});
 
 app.listen(port, () => {
     console.log(`server running on localhost:${port}`);
