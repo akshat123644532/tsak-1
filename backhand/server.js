45d871198daf1ab9973b71dc9c78ca7adf6780aa
akshat123644532 <akshatsolanki106@gmail.com>
2026-05-26 12:58:21 +0530

    mongoose connect code shift in db.js for better understending

--- backhand/server.js
+++ backhand/server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const  app = express();
+const app = express();
 const port = 5050;
+
 const cors = require("cors");
 
-const User = require("./models/User.js");
-mongoose.connect("mongodb://127.0.0.1:27017/task1")
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+const User = require("./models/User");
+const connectDB = require("./config/db");
 
 app.use(cors());
 app.use(express.json());
+connectDB();
 app.get("/",(req,res)=>{
     res.send("server is working");
 });
