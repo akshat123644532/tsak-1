89dd1ee21189b371ec0a1ff18deaa58408c66a72
akshat123644532 <akshatsolanki106@gmail.com>
2026-05-26 12:39:14 +0530

    connect with mongodb and create user schema

--- backhand/server.js
+++ backhand/server.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const  app = express();
 const port = 5050;
+const cors = require("cors");
+
+const User = require("./models/User.js");
+mongoose.connect("mongodb://127.0.0.1:27017/task1")
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
+
+app.use(cors());
+app.use(express.json());
 app.get("/",(req,res)=>{
     res.send("server is working");
+});
+app.get("/create", async (req, res) => {
+
+    const user = await User.create({
+        user: "akshat",
+        email: "solanki@gmail.com"
+    });
+
+    res.send(user);
 });
 app.listen(port,()=>{
     console.log(`server is running on locolhost at  ${port}`);
