72e93ce5a261757a5f65bf257ba3af0f00b0f5a6
akshat123644532 <akshatsolanki106@gmail.com>
2026-05-26 11:39:11 +0530

    craete basic server

--- backhand/server.js
+++ backhand/server.js
@@ -1,1 +1,9 @@
 const express = require("express");
+const  app = express();
+const port = 5050;
+app.get("/",(req,res)=>{
+    res.send("server is working");
+});
+app.listen(port,()=>{
+    console.log(`server is running on locolhost at  ${port}`);
+});
