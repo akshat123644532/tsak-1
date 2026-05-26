a639b1f2856cfd220c3bd196bcbd234e07a8c19d
akshat123644532 <akshatsolanki106@gmail.com>
2026-05-26 13:34:38 +0530

    define paths register and login paths

--- backhand/server.js
+++ backhand/server.js
@@ -14,14 +14,36 @@ connectDB();
 app.get("/",(req,res)=>{
     res.send("server is working");
 });
-app.get("/create", async (req, res) => {
-
+app.get("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+    if(!name||!email||!password){
+        return res.send("all field are required")
+    }
     const user = await User.create({
-        user: "akshat",
-        email: "solanki@gmail.com"
+
+        name,
+        email,
+        password
     });
 
     res.send(user);
+});
+app.post("/login", async (req, res) => {
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return res.send("User not found");
+    }
+
+    if (user.password !== password) {
+        return res.send("Wrong password");
+    }
+
+    res.send("Login successful");
+
 });
 app.listen(port,()=>{
     console.log(`server is running on locolhost at  ${port}`);
